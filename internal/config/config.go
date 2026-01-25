8a8c2a5c041ad54137e983009461761d71c4f12a
jwoodsiii <woods3.jessie@gmail.com>
2026-01-24 16:34:35 -0800

    updating config handling

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,25 +1,63 @@
 package config
 
-const configFileName = ".gatorconfig.json"
+import (
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
+const configFileName = "/.blogatorconfig.json"
 
 type Config struct {
 	Db_url            string `json:"db_url"`
 	Current_user_name string `json:"current_user_name"`
 }
 
-func Read() Config {
+func Read() (Config, error) {
 	// read blogatorconfig.json from home directory and marshal into Config struct
-	return Config{}
+	fp, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	file, err := os.Open(fp)
+	if err != nil {
+		return Config{}, err
+	}
+	var cfg Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+	    fmt.Println("error decoding config")
+	    return Config{}, err
+	}
+	return cfg, nil
 }
 
-func (c *Config) SetUser() {
+func (cfg *Config) SetUser() {
 	// write config struct to json after setting current_user_name
+	cfg.Current_user_name = "jwoodsiii"
 }
 
 func getConfigFilePath() (string, error) {
-	return "", nil
+	// return filepath to blogator config filez
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return home + configFileName, nil
 }
 
 func write(cfg Config) error {
+	// write config struct to json file at config file
+	fp, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	cfg_json, err := json.MarshalIndent(cfg, "", "\t")
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(fp, cfg_json, 0666); err != nil {
+		return err
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/jwoodsiii/blogator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading config:", err)
+		return
+	}
+	cfg.SetUser()
+	fmt.Printf("Current config: %v\n", cfg)
 }
