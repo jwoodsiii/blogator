1a32ebde556032eea45a287768cf2f7ece5d8b80
jwoodsiii <woods3.jessie@gmail.com>
2026-01-25 23:11:00 -0800

    adding new user register function, updating login to error on users that do not exist

--- handler_register.go
+++ handler_register.go
@@ -17,13 +17,15 @@ func handlerRegister(s *state, cmd command) error {
 
 	name := cmd.Args[0]
 
-	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: name})
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now().UTC(), UpdatedAt: time.Now().UTC(), Name: name})
 	if err != nil {
-		return fmt.Errorf("Error getting user: %v", err)
+		return fmt.Errorf("Error creating user: %v", err)
 	}
-	// TODO: add check if user already exists
 
-	s.cfg.CurrentUserName = user.Name
+	if err := s.cfg.SetUser(user.Name); err != nil {
+		return err
+	}
+
 	fmt.Printf("User: %s was created\n", user.Name)
 	log.Printf("New user: %s was created at: %s with uuid: %s\n", user.Name, user.CreatedAt, user.ID)
 
--- handler_user.go
+++ handler_user.go
@@ -1,14 +1,26 @@
 package main
-import "fmt"
+
+import (
+	"context"
+	"fmt"
+	"os"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 	user := cmd.Args[0]
-	if err := s.cfg.SetUser(user); err != nil {
+	dbUser, err := s.db.GetUser(context.Background(), user)
+	if err != nil {
+		fmt.Errorf("Error attempting to login as user: %s, %v", user, err)
+		os.Exit(1)
+	}
+
+	if err := s.cfg.SetUser(dbUser.Name); err != nil {
 		return err
 	}
-	fmt.Printf("user has been set to: %s", user)
+
+	fmt.Printf("user has been set to: %s", dbUser.Name)
 	return nil
 }
